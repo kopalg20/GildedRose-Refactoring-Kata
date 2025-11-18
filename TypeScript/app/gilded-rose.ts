076978736fcf5aa17b202aba1f883cf571aa71c6
kopalg20 <kopalgarg2005@gmail.com>
2025-11-18 19:54:32 +0530

    Initial version of refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -3,12 +3,75 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name, sellIn, quality) {
+  constructor(name: string, sellIn: number, quality: number) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
+
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+const LEGENDARY_QUALITY = 80;
+const AGED_BRIE = 'Aged Brie';
+const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+const CONJURED_ITEM_PREFIX = 'Conjured';
+
+function increaseQuality(item: Item) {
+    if(item.quality < MAX_QUALITY){
+        item.quality = item.quality + 1;
+    }
+}
+
+function decreaseQuality(item: Item) {
+    if(item.quality > MIN_QUALITY){
+        item.quality -= 1;
+    }
+}
+
+function decreaseSellIn(item: Item) {
+    item.sellIn -= 1;
+}
+
+function updateAgedBrie(item: Item) {
+    increaseQuality(item);
+    decreaseSellIn(item);
+    if(item.sellIn < 0){
+        increaseQuality(item);
+    }   
+}
+
+function updateBackstagePasses(item: Item) {
+    increaseQuality(item); 
+    if(item.sellIn < 11){
+        increaseQuality(item);
+    }
+    if(item.sellIn < 6){
+        increaseQuality(item);
+    }   
+    decreaseSellIn(item);
+    if(item.sellIn < 0){
+        item.quality = 0;
+    }
+}
+
+function updateConjuredItem(item: Item) {
+    decreaseQuality(item);
+    decreaseQuality(item);
+    decreaseSellIn(item);
+    if(item.sellIn < 0){
+      decreaseQuality(item);
+    }
+}
+
+function updateNormalItem(item: Item) {
+    decreaseQuality(item);
+    decreaseSellIn(item);
+    if(item.sellIn < 0){
+        decreaseQuality(item);
+    }
+}   
 
 export class GildedRose {
   items: Array<Item>;
@@ -19,51 +82,21 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+      if(this.items[i].name === SULFURAS) continue
+      if(this.items[i].name === AGED_BRIE){
+        updateAgedBrie(this.items[i]);
+        continue
+      } 
+      if(this.items[i].name === BACKSTAGE_PASSES){
+        updateBackstagePasses(this.items[i]);
+        continue
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if(this.items[i].name.indexOf(CONJURED_ITEM_PREFIX) === 0){
+        updateConjuredItem(this.items[i]);
+        continue
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+      updateNormalItem(this.items[i]);
     }
-
     return this.items;
   }
 }
